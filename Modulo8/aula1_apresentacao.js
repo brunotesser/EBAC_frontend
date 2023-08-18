2469ccdddab22d56255004ae3e4f73332cb3c2b7
Bruno Tesser <just.brunot@gmail.com>
2023-08-18 06:47:09 -0300

    add aula4.ja

---
+++ Modulo8/aula1_apresentacao.js
@@ -0,0 +1,9 @@
+/* 
+
+Este arquivo é apenas para deixar salvo a aula, todos as app e a prática da aula está sendo feita no aula2.html css js:
+
+
+- Vamos aplicar uma calculadora de médias font-end, através do JS, HTML e CSS
+- E também vamos conhecer o FIGMA
+
+*/
