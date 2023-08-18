45ace97f12ef7e137a6f73213115200c8265dc12
Bruno Tesser <just.brunot@gmail.com>
2023-08-18 09:46:49 -0300

    att aula2.js .html

--- Modulo8/aula5_calculandomedia.js
+++ Modulo8/aula5_calculandomedia.js
@@ -2,17 +2,7 @@
 
 
 Este arquivo é apenas para deixar salvo a aula, todos as app e a prática da aula está sendo feita no aula2.html css js
-- Nesta aula vamos
-
-
-
-
-
-
-
-
-
-
+- Nesta aula vamos trabalhar no <footer> da nossa tabela
 
 
 
