7ab7964a9d00e67ec4fb78d04d06a1be1daa734e
Bruno Tesser <just.brunot@gmail.com>
2023-08-18 16:32:53 -0300

    delet folders

--- Modulo8/aula4_insercao.js
+++
@@ -1,7 +0,0 @@
-/*
-
-Este arquivo é apenas para deixar salvo a aula, todos as app e a prática da aula está sendo feita no aula2.html css js
-
-- Aqui vamos começar a colocar lógica no nosso projeto.
-
-*/
