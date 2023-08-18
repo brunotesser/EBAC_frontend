89adf413401a5c3f2f1ea56f05e1a0476eb219b0
Bruno Tesser <just.brunot@gmail.com>
2023-08-18 15:53:42 -0300

    add task.js

---
+++ Modulo9/task.js
@@ -0,0 +1,26 @@
+/*
+
+EXERCICIO PARA REALIZAR:
+
+- Criar uma aplicação de lista de tarefas.
+- Nesta aplicação deve haver um formulário com um campo para adicionar o nome da tarefa e um botão para cadastrar a tarefa;
+- Ao submeter o formulário adicione um elemento <li> dentro de uma lista ordenada ou não;
+- Ao clicar nos itens da lista adicione um efeito que aplique uma linha em cima do texto (text-decoration: line-through);
+- Utilize o jQuery para manipular os elementos e controlar os eventos;
+
+- Criar uma branch chamada exercício_jquery no repositório do curso e nos envie o link através da plataforma.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
