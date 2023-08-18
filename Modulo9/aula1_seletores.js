ac45aed9ee041ef04aadfb49a696ddbfeb8fc37d
Bruno Tesser <just.brunot@gmail.com>
2023-08-18 15:34:26 -0300

    add aula5.js

--- Modulo9/aula1_seletores.js
+++ Modulo9/aula1_seletores.js
@@ -4,13 +4,25 @@ $(document).ready(function () {
 
   })
 
-  $('#botao-cancel').click(function(){
+  $('#botao-cancel').click(function () {
     $('form').slideUp();
   })
 
   $('form').on('submit', function (e) {
-    console.log("submit");
     e.preventDefault();
+    const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+    const novoItem = $('<li style="display:none"> </li>');
+    $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+    $(`<div class="overlay-imagem-link">
+    <a href="$(enderecoDaNovaImage)" target="_blank" title="Ver imagem em tamanho real">
+    Ver imagem em tamanho real
+    </a>
+    </div>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn();
+    $("#endereco-imagem-nova").val(' ')
+
+
   })
 
 })
