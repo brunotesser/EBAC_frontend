5f45c978230de4bf3ee0420dcfcf987e27fc9790
Bruno Tesser <just.brunot@gmail.com>
2023-08-18 09:48:11 -0300

    add aula6.js

---
+++ Modulo8/aula6_deploy.js
@@ -0,0 +1,9 @@
+/*
+
+
+Este arquivo é apenas para deixar salvo a aula, todos as app e a prática da aula está sendo feita no aula2.html css js
+- Aqui vamos fazer publicação do projeto no GITHUB em um repositório chamado (projeto_calculadora_medias)
+
+
+
+*/
