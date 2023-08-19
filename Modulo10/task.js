8f2271a285854ead2ac6030a0d488675d0e3d9b1
Bruno Tesser <just.brunot@gmail.com>
2023-08-19 09:36:14 -0300

    finish task

--- Modulo10/task.js
+++ Modulo10/task.js
@@ -1,9 +1,15 @@
 /* 
  
 1- Criar um formulário de cadastro.
-2- Este formulário deverá conter os campos: nome completo, e-mail, telefone, CPF, endereço completo e CEP;
-3- Utilize o plugin jQuery Mask Plugin para aplicar máscara aos campos de CPF e CEP;
-4- Criar uma branch chamada exercício_plugins_jquery e mandar o link 
+
+2- Este formulário deverá conter os campos: nome completo, 
+e-mail, telefone, CPF, endereço completo e CEP;
+
+3- Utilize o plugin jQuery Mask Plugin para aplicar 
+máscara aos campos de CPF e CEP;
+
+4- Criar uma branch chamada exercício_plugins_jquery e 
+mandar o link 
 
 
 
---
+++ Modulo10/task/js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function (){
+  $('#cpf').mask('000.000.000-00');
+  $('#cep').mask('00000-000');
+})
