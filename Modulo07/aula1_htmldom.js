7207b9d462ff5d2a0baf7588df3bd68c1647d000
Bruno Tesser <just.brunot@gmail.com>
2023-08-19 09:47:15 -0300

    delete folder

--- Modulo07/aula1_htmldom.js
+++
@@ -1,7 +0,0 @@
-// DOM = Document Object Model
-// window.location.href (pegar url)
-// window.location.pathname (pega tudo depois do dominigo site.com/....)
-// window.history.back () (isso fara com que te leve para pagina anterior)
-// document.title = "Olá teste" (isso faz com que você mude o title do documento)
-// document.body (isso fará com que você tenha acesso a todo html do body)
-// document.body.innerHTML = "<h1> Olá DOM </h1>" (isso fará com que você tenha acesso e escreva na pagina)
