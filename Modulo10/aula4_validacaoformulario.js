74c9dd6c74902d5f41a227a6023c53169eda4e2f
Bruno Tesser <just.brunot@gmail.com>
2023-08-19 08:04:30 -0300

    add .mask jquery and validate

--- Modulo10/aula4_validacaoformulario.js
+++ Modulo10/aula4_validacaoformulario.js
@@ -3,5 +3,7 @@
 Tudo vai estar sendo feito através do index.html main.css e main.js
 
 https://kenwheeler.github.io/slick/ (usar biblioteca pela cdn)
+https://igorescobar.github.io/jQuery-Mask-Plugin/
+https://jqueryvalidation.org/
 
 */
