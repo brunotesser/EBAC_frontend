4ae07a0e6278190e3b8780ae4da9a81a8151fa76
Bruno Tesser <just.brunot@gmail.com>
2023-08-21 20:30:06 -0300

    add aula4.js

--- Modulo12/aulas/aula2_grids.js
+++ Modulo12/aulas/aula2_grids.js
@@ -1,4 +1,6 @@
 /*
+> Aprendemos a criar através do atalha a usar o ROW e também COLUNAS no bootstrap
+        <img src="https://via.placeholder.com/180x180" alt="">
 
 
 
--- Modulo12/aulas/aula3_tipografia.js
+++ Modulo12/aulas/aula3_tipografia.js
@@ -1,5 +1,6 @@
 /*
-
+- O bootstrap nos disponibiliza ja uma fonte customizada: h1, p, mark, <p class="h1">, h1 class="display-1"; etc
+- 
 
 
 */
